ee4bf44a9be1e04a280d12e0ce76f158741fc529
夏明伟 <xiamwei01@163.com>
2016-03-18 10:01:18 +0800

    Initial Commit

---
+++ myLoading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myLoading
+//
+//  Created by Sino on 16/3/18.
+//  Copyright © 2016年 夏明伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myLoading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myLoading
+//
+//  Created by Sino on 16/3/18.
+//  Copyright © 2016年 夏明伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
